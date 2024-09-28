e64c7bdfac54d9c46e2244e9d14a7afdbc495580
Julia <julia.r.lindgren@gmail.com>
2024-09-28 18:43:43 +0200

    Complete core

--- src/index.js
+++ src/index.js
@@ -3,4 +3,39 @@ console.log(data);
 
 //You can start simple and just render a single 
 //pokemon card from the first element
-console.log(data[0]);
+
+const cardList = document.querySelector(".cards");
+
+function renderPokemonCards() {
+    cardList.innerHTML = '';
+
+    data.forEach((pokemon) => {
+        const li = document.createElement('li');
+        li.className = 'card'
+
+        const h2 = document.createElement('h2');
+        h2.className = 'card--title';
+        h2.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+        li.appendChild(h2);
+
+        const img = document.createElement('img');
+        img.width = '256';
+        img.className = 'card--img';
+        img.src = pokemon.sprites.front_default;
+        li.appendChild(img);
+
+        const ul = document.createElement('ul');
+        ul.className = 'card--text';
+        pokemon.stats.forEach((stat) => {
+            const statli = document.createElement('li');
+            statli.innerText = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`
+            ul.appendChild(statli);
+        })
+        li.appendChild(ul);
+
+        cardList.appendChild(li);
+    });
+}
+
+renderPokemonCards();
+
